47e762652833c14e1a7233217dc4384c2634f95f
SigurdMW <sigurdmwahl@gmail.com>
2017-04-17 10:24:56 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './main.global.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
